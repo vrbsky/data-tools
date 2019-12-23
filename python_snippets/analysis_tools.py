412df12985781253af750e9fcdc558d2cecd0df8
Ladislav Vrbsky <ladislav.vrbsky@gmail.com>
2019-12-23 15:54:40 -0300

    describe_data function to analysis

--- python_snippets/analysis_tools.py
+++ python_snippets/analysis_tools.py
@@ -1,2 +1,13 @@
 def print_all_columns(df):
-  return HTML('<br />'.join(str(y) for y in df.columns))
+    return HTML('<br />'.join(str(y) for y in df.columns))
+
+def describe_data(df):
+    print("Data Types:")
+    print(df.dtypes)
+    print("Rows and Columns:")
+    print(df.shape)
+    print("Column Names:")
+    print(df.columns)
+    print("Null Values:")
+    print(df.apply(lambda x: sum(x.isnull()) / len(df)))
+
--- python_snippets/describe_data.py
+++
@@ -1,9 +0,0 @@
-def describe_data(df):
-    print("Data Types:")
-    print(df.dtypes)
-    print("Rows and Columns:")
-    print(df.shape)
-    print("Column Names:")
-    print(df.columns)
-    print("Null Values:")
-    print(df.apply(lambda x: sum(x.isnull()) / len(df)))
